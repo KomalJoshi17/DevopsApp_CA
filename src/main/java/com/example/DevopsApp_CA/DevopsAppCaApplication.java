1721c97f3ddd6e7291bdff1a7ad5ca8e45f12afe
Komal Joshi <moudgillkomal6@gmail.com>
2026-05-08 21:52:05 +0530

    Initial commit - DevOps project

---
+++ src/main/java/com/example/DevopsApp_CA/DevopsAppCaApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevopsApp_CA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAppCaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAppCaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DevopsApp_CA/DevopsAppCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevopsApp_CA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAppCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
