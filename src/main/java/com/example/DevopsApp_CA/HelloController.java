5b97f3b94554ef7c5800fbe737d9ea74a292e491
Komal Joshi <moudgillkomal6@gmail.com>
2026-05-27 12:10:21 +0530

    tittle updated

--- src/main/java/com/example/DevopsApp_CA/HelloController.java
+++ src/main/java/com/example/DevopsApp_CA/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
 
         <head>
 
-            <title>DevOps CI/CD Pipeline</title>
+            <title>DevOps CI/CD Pipeline 👋</title>
 
             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 
